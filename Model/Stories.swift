ec54e0c7c4c53c054b218c5eb617d61e88c8a26a
Andy Patterson <101858100+91ap@users.noreply.github.com>
2025-02-18 17:52:31 -0600

    Update Stories.swift

--- Model/Stories.swift
+++ Model/Stories.swift
@@ -1,25 +1,2 @@
-//
-//  Stories.swift
-//  Destiniii
-//
-//  Created by apat on 2/14/25.
-//
-import Foundation
 
-struct StoryBrain {
-    
-    
-    
-    let stories = [
-        Story(storyTitle: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
-        Story(storyTitle: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
-        Story(storyTitle: "You find a treasure chest", choice1: "Open it", choice2: "check for traps"),
-    ]
-    
-    var questionNumber = 0
-    
-   mutating func getStoryTitle() {
-        
-        
-    }
-}
+
